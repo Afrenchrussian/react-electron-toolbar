e70792911fb08032396da283a2c9ace536f3e7db
Kieran Lewin <afrenchrussian@gmail.com>
2020-09-02 11:56:28 +0100

    v0.1.1

---
+++ example/src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// @ts-ignore
+import Toolbar from 'react-electron-toolbar'
+
+ReactDOM.render(
+  <React.StrictMode>
+      <Toolbar/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
