a004bc43cbc225e97ad4a537c88d913e4b4f920b
Kieran Lewin <kieran.lewin-cic-uk@ibm.com>
2020-09-08 18:35:37 +0100

    added clicking to button

--- dist/Components/Windows/ToolbarWindows.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-import '../../Css/ToolbarWindows.css';
-export interface Props {
-    color?: string;
-}
-export declare function ToolbarWindows({ color }: Props): JSX.Element;
